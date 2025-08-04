d2871182e6d3b8ac69b9c05e868397b9ac285a06
karentejaxun <karentejaxun@gmail.com>
2025-08-03 23:45:36 -0600

    primer commit

--- app/routes/estudiante.routes.js
+++ app/routes/estudiante.routes.js
@@ -17,5 +17,5 @@ module.exports = app => {
     router.delete("/delete/", Estudiante.deleteAll);
     // Podemos utilizar como una ocpion app.use("EndPoint",router" para simplicar el URI
     // Ej.  http://localhost:Puerto/api/cliente/
-    app.use("/api/customer", router);
+    app.use("/api/estudiante", router);
 };
